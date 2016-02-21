a1d080a19ef0f8853a4b8f3345d19aedb175c08c
Joe Cianflone <joec@jkdesign.com>
2016-02-21 14:06:44 -0500

    WIP spriting

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,16 +4,32 @@ var gulp       = require('gulp'),
     imagemin   = require('gulp-imagemin'),
     pngquant   = require('imagemin-pngquant');
     livereload = require('gulp-livereload'),
+    sprity     = require('sprity'),
+    del        = require('del'),
+    gulpif     = require('gulp-if'),
     config     = require('../config.js');
 
-gulp.task('imagemin', function () {
-   gulp.src(config.src.imgs + '**/*.*')
-      .pipe(plumber({errorHandler: notify.onError("Imagemin Error:\n<%= error.message %>")}))
-      .pipe(imagemin({
-         progressive: true,
-         svgoPlugins: [{removeViewBox: false}],
-         use: [pngquant()]
-      }))
-      .pipe(gulp.dest(config.dest.imgs))
-      .pipe(livereload());
-});
+
+// gulp.task('imageProcess', function () {
+//    return gulp.src(config.src.imgs + '**/*')
+//       .pipe(plumber({errorHandler: notify.onError("Imagemin Error:\n<%= error.message %>")}))
+//       .pipe(imagemin({
+//          progressive: true,
+//          svgoPlugins: [{removeViewBox: false}],
+//          use: [pngquant()]
+//       }))
+//       .pipe(gulp.dest(config.dest.imgs))
+//       .pipe(livereload());
+// });
+
+// gulp.task('spriteGenerator', ['imageProcess'], function(cb) {
+//   return sprity.src({
+//     src: config.dest.imgs + '**/*.{png,jpeg,jpg}',
+//     style: config.src.sass + 'modules/_sprite.scss',
+//     processor: 'sass',
+//     split: true
+//   })
+//   .on('error', function (err) {cb();})
+//   .pipe(plumber({errorHandler: notify.onError("Sprity Error:\n<%= error.message %>")}))
+//   .pipe(gulpif('*.png', gulp.dest(config.dest.imgs), gulp.dest(config.src.sass+'modules')));
+// });
