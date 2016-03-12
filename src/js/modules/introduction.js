2b1cb709341f6b8ace74e1c3378479630d07e2b4
Joe Cianflone <joe@cianflone.co>
2016-03-11 20:19:35 -0500

    removed some unneeded files

--- src/js/modules/introduction.js
+++ src/js/modules/introduction.js
@@ -6,11 +6,15 @@ App.Modules.Introduction = function () {
       console.log("HELLO");
    };
 
+   var world = function(data) {
+      console.log(data);
+   };
+
    return {
       init: function() { return this; },
       events: function() {
          Events.bind("click", ".js-mouse-test").to(hello, this);
-
+         Events.bind("keypress").to(world, this);
          return this;
       }
    };
