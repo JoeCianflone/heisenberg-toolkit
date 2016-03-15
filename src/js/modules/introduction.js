c0820ff544328abf8e3b20bec2d4a20988bb08c6
Joe Cianflone <joe@cianflone.co>
2016-03-15 13:12:46 -0400

    remove some nonsense testing stuff, bump version, prune deps

--- src/js/modules/introduction.js
+++ src/js/modules/introduction.js
@@ -2,19 +2,9 @@ App.Modules = App.Modules || {};
 App.Modules.Introduction = function () {
    var o = { };
 
-   var hello = function(data) {
-      console.log("HELLO");
-   };
-
-   var world = function(data) {
-      console.log(data);
-   };
-
    return {
       init: function() { return this; },
       events: function() {
-         Events.bind("click", ".js-mouse-test").to(hello, this);
-         Events.bind("keypress").to(world, this);
          return this;
       }
    };
